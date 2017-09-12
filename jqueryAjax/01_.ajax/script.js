fc4c45b510506e1d120158fde9bd78f0f1dad97a
李达康 <3075139617@qq.com>
2017-09-12 21:58:58 +0800

    Update script.js

--- jqueryAjax/01_.ajax/script.js
+++ jqueryAjax/01_.ajax/script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#btn").click(function(){
-		htmlobj = $.ajax({url:"http://www.w3school.com.cn/jquery/test1.txt",async:false});
+		htmlobj = $.ajax({url:"test1.txt",async:false});
 		$("#myDiv").html(htmlobj.responseText);
 	});
 });
